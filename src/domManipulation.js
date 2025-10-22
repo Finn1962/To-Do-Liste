43b034effe3f03be4f86210719893c00b83b86e0
Finn Schmidt <finn.schmidt@brainworxx.de>
2025-10-22 15:55:12 +0200

    CSS und hinzufügen von Projekten funktionieren

--- src/domManipulation.js
+++ src/domManipulation.js
@@ -1,18 +1,25 @@
+import iconBearbeiten from './images/iconBearbeiten.svg';
+import iconLöschen from './images/iconLöschen.svg';
 import {nutzerProjekte} from "./nutzerProjekte"
-
 
 
 export class domManipulation {
     
     static projekteInDomÜbernehmen() {
-        const projekteUndÜberschrift = document.getElementById("projekteUndÜberschrift");
-        //projekteUndÜberschrift.innerHTML = "";
+        const ausgewählterHTMLBereich = document.getElementById("projekteBereich");
+        ausgewählterHTMLBereich.innerHTML = "";
+        
         if (nutzerProjekte.length>0) {
             for (let i = 0; i<nutzerProjekte.length; i++) {
-                //const neuesProjekt = document.createElement('p');
-                //neuesProjekt.textContent = nutzerProjekte[i].titel;
-                //neuesProjekt.classList.add("projekt");
-                //projekteUndÜberschrift.appendChild(neuesProjekt);
+                const titel = nutzerProjekte[i].titel;
+                const htmlCodeProjekte =             
+                    `<div class="projektContainer">
+                        <p>${titel}</p>
+                        <img src="${iconBearbeiten}" width="15px">
+                        <img src="${iconLöschen}" width="15px">
+                    </div>`
+                ;
+                ausgewählterHTMLBereich.innerHTML += htmlCodeProjekte;
             }
         }
     }
